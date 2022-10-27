3e62f1d9b4f47bf615b638791694712129637e49
Ale-1407 <alessiacarrera07@gmail.com>
2022-10-27 17:28:40 +0200

    esercizio palindroma completo

--- js/main.js
+++ js/main.js
@@ -3,3 +3,24 @@ Palidroma
 Chiedere all’utente di inserire una parola
 Creare una funzione per capire se la parola inserita è palindroma
 */
+
+//chiedo all'utente una parola
+let parola = prompt('Inserisci una parola');
+
+//verifico se la parola inserita è palindroma
+function palindroma(){
+
+    let parolaReverse = '';
+
+    //mettiamo i > = 0 perchè deve partire al contrario
+    for(let i = parola.length - 1; i >= 0; i--){
+        parolaReverse += parola[i]; 
+    }
+   return parolaReverse;
+}
+
+if(parola == palindroma()){
+    console.log(`La parola è palindroma`);
+}else{
+    console.log('La parola NON è palindroma');
+}
